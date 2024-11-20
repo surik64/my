fc3599a47db05af4acd6d85b3ded7a59986b8938
Suresh <sureshkommu112@gmail.com>
2024-11-20 10:40:58 +0530

    adding endpint

--- app.py
+++ app.py
@@ -4,19 +4,27 @@ from flask import Flask
 
 # Flask constructor takes the name of 
 # current module (__name__) as argument.
+# This initializes the Flask application.
 app = Flask(__name__)
 
 # The route() function of the Flask class is a decorator, 
 # which tells the application which URL should call 
 # the associated function.
-@app.route('/')
+# Define the endpoint
+# This defines a route (or endpoint) /helloworld that will be triggered when a GET request is made.
+@app.route('/helloworld', methods=['GET'])
 # ‘/’ URL is bound with hello_world() function.
 def hello_world():
-    return 'Hello World'
+    #The function that returns the response "Hello, World!".
+    return 'Hello, World!'
 
 # main driver function
 if __name__ == '__main__':
 
     # run() method of Flask class runs the application 
     # on the local development server.
-    app.run()
+    # This starts the app in debug mode, which helps in development by auto-reloading on code changes.
+    # Run the Flask application
+    app.run(debug=True)
+
+# python app.py -> run this commomnd in terminal to run application
